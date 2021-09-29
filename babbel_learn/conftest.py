1d12f49bc44a675ba0caa3c74786de000de9afab
himanshu <himanshu.das@infra.market>
2021-09-29 23:28:00 +0530

    init

---
+++ babbel_learn/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from babbel_learn.users.models import User
+from babbel_learn.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
