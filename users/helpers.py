b673f7df8ad5c00f0a8ddae790ef1e4c2ad5de2c
hd <himanshu.das@infra.market>
2021-10-07 15:28:51 +0530

    fixed token calc

--- users/helpers.py
+++ users/helpers.py
@@ -8,7 +8,7 @@ def token_auth(func):
             return Response("UnAuthorized", status=401)
         else:
             token = request.META.get("HTTP_AUTHORIZATION").split(" ")[-1]
-            user = UserData.objects.raw("SELECT * FROM users_userdata WHERE id in (SELECT id FROM users_token WHERE key=%s)", [token])
+            user = UserData.objects.raw("SELECT * FROM users_userdata WHERE id in (SELECT user_id FROM users_token WHERE key=%s)", [token])
             if len(user)<1:
                 return Response("UnAuthorized", status=401)
             else:
